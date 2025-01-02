161db1d7b918b378b34160654e8a9460c3757c10
FlorianGmz <florian.g.code@gmail.com>
2025-01-02 16:18:42 +0100

    fir

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import {
   contactLoader,
   footerLoader,
 } from "./services/loaders";
+import { Analytics } from "@vercel/analytics/next";
 
 const router = createBrowserRouter([
   {
@@ -63,6 +64,7 @@ function App() {
     <>
       <GlobalStyles />
       <RouterProvider router={router} />
+      <Analytics />
     </>
   );
 }
